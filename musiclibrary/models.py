893a26dfed62ad60425d70401e0e5619ebf87360
Adam Frisch <afrisch88@outlook.com>
2022-03-11 10:54:54 -0600

    added "like" into models and serializer

--- musiclibrary/models.py
+++ musiclibrary/models.py
@@ -9,3 +9,4 @@ class Song(models.Model):
     album = models.CharField(max_length=255)
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
+    like = models.IntegerField(0)
